90b7cf89f31fcf216b15ecdee3f93ebb905c22bf
James Zhu <zhujian0805@gmail.com>
2016-12-30 16:51:30 +0800

    This commit has the following change(s): ------------------------------------------------  M register.asm ?? demo ?? register.o ------------------------------------------------

--- assembly_registers/register.asm
+++ assembly_registers/register.asm
@@ -14,7 +14,8 @@ _start:           ;tell linker entry point
    mov   eax,4    ;system call number (sys_write)
    int   0x80     ;call kernel
 
-   mov   ecx,0xa   ;message to write
+   mov   edx,1    ;message length
+   mov   ecx,nl   ;message to write
    mov   ebx,1    ;file descriptor (stdout)
    mov   eax,4    ;system call number (sys_write)
    int   0x80     ;call kernel
@@ -26,3 +27,4 @@ section  .data
 msg db 'Displaying 9 stars',0xa ;a message
 len equ $ - msg  ;length of message
 s2 times 9 db '*'
+nl db 0xa
